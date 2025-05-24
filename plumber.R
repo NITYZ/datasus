54a829fa1f4b26745ffccb7ebfd3dde6f6c5a2ac
NITYZ <luisgustavomf27@gmail.com>
2025-05-24 12:31:17 -0300

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -3,10 +3,7 @@ library(plumber)
 library(microdatasus)
 library(dplyr)
 
-#* @get /ping
-function() {
-  list(status = "ok", msg = "pong")
-}
+#* @apiTitle PubMack microdatasus API
 
 #* @get /sih
 #* @param uf Estado (ex: "SP")
@@ -21,5 +18,57 @@ function(uf, ano) {
   head(df, 10)
 }
 
-# No final do arquivo:
+#* @get /sim
+#* @param uf Estado (ex: "SP")
+#* @param ano Ano (ex: 2022)
+function(uf, ano) {
+  df <- fetch_datasus(
+    year_start = as.integer(ano),
+    year_end = as.integer(ano),
+    uf = uf,
+    information_system = "SIM-DO"
+  )
+  head(df, 10)
+}
+
+#* @get /sinasc
+#* @param uf Estado (ex: "SP")
+#* @param ano Ano (ex: 2022)
+function(uf, ano) {
+  df <- fetch_datasus(
+    year_start = as.integer(ano),
+    year_end = as.integer(ano),
+    uf = uf,
+    information_system = "SINASC"
+  )
+  head(df, 10)
+}
+
+#* @get /sia
+#* @param uf Estado (ex: "SP")
+#* @param ano Ano (ex: 2022)
+function(uf, ano) {
+  df <- fetch_datasus(
+    year_start = as.integer(ano),
+    year_end = as.integer(ano),
+    uf = uf,
+    information_system = "SIA-PA"
+  )
+  head(df, 10)
+}
+
+#* @get /cnes
+#* @param uf Estado (ex: "SP")
+#* @param ano Ano (ex: 2022)
+function(uf, ano) {
+  df <- fetch_datasus(
+    year_start = as.integer(ano),
+    year_end = as.integer(ano),
+    uf = uf,
+    information_system = "CNES-ST"
+  )
+  head(df, 10)
+}
+
+# Start API (obrigatório pro Render)
 pr() %>% pr_run(host = "0.0.0.0", port = as.numeric(Sys.getenv("PORT", 8000)))
